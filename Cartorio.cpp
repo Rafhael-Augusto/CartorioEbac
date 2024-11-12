03c1ae3fd09966547d6dd519b4fd18e649d1efa4
Rafhael Augusto <RafhaelAlvesPrado@gmail.com>
2024-11-11 21:10:20 -0300

    Opção de saída
    Incluso opção de saída.

--- Cartorio.cpp
+++ Cartorio.cpp
@@ -129,7 +129,8 @@ int main(){
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n");
+		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: ");
 	
 		scanf("%d", &opcao);     //Scanear oque a pessoa disse e mudar "opcao" pro valor digitado e continuar script
@@ -146,6 +147,11 @@ int main(){
 			
 			case 3:    // "Ex: if (case == 3) "
 			deletar();
+			break;
+			
+			case 4:
+			printf("Saindo...Aperte qualquer botão para fechar o painel.\n");
+			return 0;
 			break;
 			
 			default:
